95eaf72c475651f0487597e6603ca410e799e7fe
Vikas Kumar <vikumar162006@gmail.com>
2026-04-05 16:48:37 +0530

    Update db connection for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect('mongodb://127.0.0.1:27017/Delicia')
+    .connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/Delicia')
     .then(() => console.log("✅ DB Connected"))
     .catch((err) => console.error("❌ DB Connection Error:", err));
 };
